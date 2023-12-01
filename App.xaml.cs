11ffcbb7a06bf7b20cb1faef7d2519fff4f75bb8
Raluca Mocanu <raluca.mocanu@stud.ubbcluj.ro>
2023-12-01 12:01:00 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,28 @@
-﻿namespace Mocanu_Raluca_Lab7;
+﻿using Mocanu_Raluca_Lab7.Data;
+using System.IO;
+using System;
+
+
+namespace Mocanu_Raluca_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
